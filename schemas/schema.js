c08454e4381e23fc12269e714a80cd0b16c55f16
AnonymousPro139 <anonymouspro139@gmail.com>
2021-10-03 16:55:50 +0800

    lessonPic nemew

--- schemas/schema.js
+++ schemas/schema.js
@@ -69,6 +69,11 @@ export default createSchema({
           name: "title",
           type: "string",
         },
+        {
+          title: "Хичээлийн зураг",
+          name: "lessonPic",
+          type: "image",
+        },
       ],
     },
     {
